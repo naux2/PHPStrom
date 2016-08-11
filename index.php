9b80f92f239354858b0dfec637dbca2c2df4c044
naux2 <hhuwxx@126.com>
2016-08-11 14:55:44 +0800

    first php file with Chorme Debug

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>flex login</title>
+</head>
+
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jin
+ * Date: 2016/8/11
+ * Time: 14:07
+ */
+$instr = "hello world9";
+$return = "this";
+$return.='<a>总计接收'.$instr.'条待入库条目</a>';
+echo $return;
+
+?>
+</body>
+</html>
